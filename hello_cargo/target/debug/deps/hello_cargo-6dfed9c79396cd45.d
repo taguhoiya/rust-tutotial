9c842796c4e6fc67c733daf7669d40db00062898
taguhoiya <kawasaki.takudai@moneyforward.co.jp>
2023-01-18 17:20:23 +0900

    cargo run

---
+++ hello_cargo/target/debug/deps/hello_cargo-6dfed9c79396cd45.d
@@ -0,0 +1,5 @@
+/Users/kawasaki.takudai/PrivateProject/rust-tutorial/hello_cargo/target/debug/deps/hello_cargo-6dfed9c79396cd45: src/main.rs
+
+/Users/kawasaki.takudai/PrivateProject/rust-tutorial/hello_cargo/target/debug/deps/hello_cargo-6dfed9c79396cd45.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/kawasaki.takudai/PrivateProject/rust-tutorial/hello_cargo/target/debug/hello_cargo: /Users/kawasaki.takudai/PrivateProject/rust-tutorial/hello_cargo/src/main.rs
