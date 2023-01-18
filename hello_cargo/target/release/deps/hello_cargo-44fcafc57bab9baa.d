3461768205e541ee53c3a9595d1191e9ed479e83
taguhoiya <kawasaki.takudai@moneyforward.co.jp>
2023-01-18 17:21:46 +0900

    cargo build --release

---
+++ hello_cargo/target/release/deps/hello_cargo-44fcafc57bab9baa.d
@@ -0,0 +1,5 @@
+/Users/kawasaki.takudai/PrivateProject/rust-tutorial/hello_cargo/target/release/deps/hello_cargo-44fcafc57bab9baa: src/main.rs
+
+/Users/kawasaki.takudai/PrivateProject/rust-tutorial/hello_cargo/target/release/deps/hello_cargo-44fcafc57bab9baa.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/release/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/kawasaki.takudai/PrivateProject/rust-tutorial/hello_cargo/target/release/hello_cargo: /Users/kawasaki.takudai/PrivateProject/rust-tutorial/hello_cargo/src/main.rs
